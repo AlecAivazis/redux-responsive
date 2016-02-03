a1d5e767800dd4895d6568dc5a2c8fac113cac44
Monte Mishkin <monte@mishkin.com>
2016-02-02 23:28:25 -0800

    added tests for `createResponsiveStoreEnhancer`

---
+++ tests/test_createResponsiveStoreEnhancer.js
@@ -0,0 +1,50 @@
+// third party imports
+import isFunction from 'lodash/lang/isFunction'
+// local imports
+import createResponsiveStoreEnhancer from 'util/createResponsiveStoreEnhancer'
+
+
+describe('createResponsiveStoreEnhancer', function () {
+    it('returns a function when given a throttle time', function () {
+        expect(isFunction(createResponsiveStoreEnhancer(500))).to.be.true
+    })
+
+    it('returns a function when not given a throttle time', function () {
+        expect(isFunction(createResponsiveStoreEnhancer())).to.be.true
+    })
+
+    describe('the returned store enhancer', function () {
+        let enhancer
+
+        beforeEach(function () {
+            enhancer = createResponsiveStoreEnhancer()
+        })
+
+        it('returns a function', function () {
+            expect(isFunction(enhancer())).to.be.true
+        })
+
+        describe('the returned (enhanced) `createStore`', function () {
+            let createStoreSpy
+            let enhancedCreateStore
+
+            beforeEach(function () {
+                function fakeCreateStore() {
+                    return {
+                        dispatch: () => {},
+                    }
+                }
+
+                createStoreSpy = sinon.spy(fakeCreateStore)
+                enhancedCreateStore = enhancer(createStoreSpy)
+            })
+
+            it('calls the `createStore` given to the enhancer', function () {
+                enhancedCreateStore()
+                expect(createStoreSpy).to.have.been.calledOnce
+            })
+        })
+    })
+
+    it('could use some more tests')
+})
