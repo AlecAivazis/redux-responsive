c6d25c95c66e166036b25f9f49958bbbf17d9507
Alec Aivazis <alec@aivazis.com>
2016-02-18 14:03:04 -0800

    action type was added to root export ; fixes #15

--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
 // local imports
 import createResponsiveStateReducer from './util/createResponsiveStateReducer'
 import createResponsiveStoreEnhancer from './util/createResponsiveStoreEnhancer'
-
+import {CALCULATE_RESPONSIVE_STATE} from './actions/types'
 
 export default {
     createResponsiveStateReducer,
     createResponsiveStoreEnhancer,
+    CALCULATE_RESPONSIVE_STATE,
     // provide default responsive state reducer
     responsiveStateReducer: createResponsiveStateReducer(),
     // provide default responsive store enhancer
