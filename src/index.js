3976fa3e29ae261cc0ed21dedb38b7c8cf29b536
alec aivazis <alec@aivazis.com>
2016-04-16 00:25:21 -0700

    calculate state action creator is now exported from package

--- src/index.js
+++ src/index.js
@@ -2,8 +2,10 @@
 import createResponsiveStateReducer from './util/createResponsiveStateReducer'
 import createResponsiveStoreEnhancer from './util/createResponsiveStoreEnhancer'
 import {CALCULATE_RESPONSIVE_STATE} from './actions/types'
+import {calculateResponseState} from './actions/creators'
 
 export default {
+    calculateResponseState,
     createResponsiveStateReducer,
     createResponsiveStoreEnhancer,
     CALCULATE_RESPONSIVE_STATE,
