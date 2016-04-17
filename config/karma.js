a2b27e121f0b221c678f0fa2575881fa768174ef
Alec Aivazis <alec@aivazis.com>
2016-04-16 22:51:07 -0700

    first pass at travis config

--- config/karma.js
+++ config/karma.js
@@ -55,7 +55,7 @@ module.exports = function (config) {
         // test results reporter to use
         // possible values: 'dots', 'progress'
         // available reporters: https://npmjs.org/browse/keyword/karma-reporter
-        reporters: ['mocha'],
+        reporters: ['mocha', 'coverage'],
 
         // web server port
         port: 9876,
@@ -71,6 +71,9 @@ module.exports = function (config) {
             // 'Firefox',
             // 'Safari',
         ],
+        coverageReporter: {
+            type: 'lcov'
+        },
     })
 }
 
