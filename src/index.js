5d10da5b8e3221043efdac827618b3b9bfabff53
Alec Aivazis <alec@aivazis.com>
2016-05-14 11:44:19 -0700

    fixed typo in root level imports

--- src/index.js
+++ src/index.js
@@ -2,10 +2,10 @@
 import createResponsiveStateReducer from './util/createResponsiveStateReducer'
 import createResponsiveStoreEnhancer from './util/createResponsiveStoreEnhancer'
 import {CALCULATE_RESPONSIVE_STATE} from './actions/types'
-import {calculateResponseState} from './actions/creators'
+import {calculateResponsiveState} from './actions/creators'
 
 export default {
-    calculateResponseState,
+    calculateResponsiveState,
     createResponsiveStateReducer,
     createResponsiveStoreEnhancer,
     CALCULATE_RESPONSIVE_STATE,
