622bf0c55c3e3b25ae7b4c39caf692f161eff0a8
Peter Radcliffe <peter.radcliffe@cloudsherpas.com>
2016-11-14 12:17:56 +0000

    Removed unused innerWidth arg to satisfy linting errors.

--- src/actions/creators/calculateResponsiveState.js
+++ src/actions/creators/calculateResponsiveState.js
@@ -7,8 +7,6 @@ import {CALCULATE_RESPONSIVE_STATE} from '../types'
  * responsive state.
  * @arg {object} window - Any window-like object (has keys `innerWidth` and
  * `matchMedia`).
- * @arg {number} window.innerWidth - The value for the browser width (to pass to
- * the responsive state reducer logic).  See browser global `window.innerWidth`.
  * @arg {function} window.matchMedia - The method with which to match media
  * queries (to pass to the responsive sate reducer logic).  See global
  * `window.matchMedia`.
@@ -16,9 +14,8 @@ import {CALCULATE_RESPONSIVE_STATE} from '../types'
  * `CALCULATE_RESPONSIVE_STATE`, and will be directly given the two keys taken
  * from the `window` argument.
  */
-export default ({innerWidth, innerHeight, matchMedia} = {}) => ({
+export default ({innerHeight, matchMedia} = {}) => ({
     type: CALCULATE_RESPONSIVE_STATE,
-    //innerWidth,
     innerWidth: document.documentElement.clientWidth,
     innerHeight,
     matchMedia,
