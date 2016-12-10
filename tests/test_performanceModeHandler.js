c485e2779817aaaffa35c7e8b72f959f583323f1
Alec Aivazis <alec@aivazis.com>
2016-12-10 15:06:34 -0800

    remove unused tests

---
+++ tests/test_performanceModeHandler.js
@@ -0,0 +1,33 @@
+
+
+// third party imports
+import isFunction from 'lodash/isFunction'
+// local imports
+import createResponsiveStoreEnhancer from 'util/createResponsiveStoreEnhancer'
+
+
+describe('createResponsiveStoreEnhancer', function () {
+    it('respects the calculateInitialState option', function () {
+        // const dispatchSpy1 = sinon.spy()
+        // const dispatchSpy2 = sinon.spy()
+
+        // addEventHandlers(
+        //     {dispatch: dispatchSpy1},
+        //     {throttleTime: 100, calculateStateInitially: true}
+        // )
+
+        // // should have triggered our dispatch spy exactly once
+        // expect(dispatchSpy1).to.have.been.calledOnce
+
+        // addEventHandlers(
+        //     {dispatch: dispatchSpy2},
+        //     {calculateStateInitially: false}
+        // )
+
+        // // should not dispatch recalculation
+        // expect(!dispatchSpy2.called)
+    })
+
+
+    it('could use some more tests')
+})
