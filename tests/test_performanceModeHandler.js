94ec1713114d96a2b9c8a9b1a5cad290e6f2c02f
Alec Aivazis <alec@aivazis.com>
2016-12-11 14:13:34 -0800

    added test for performance mode initial state

--- tests/test_performanceModeHandler.js
+++ tests/test_performanceModeHandler.js
@@ -1,12 +1,37 @@
-
-
-// third party imports
-import isFunction from 'lodash/isFunction'
+// external imports
+import {createStore, combineReducers} from 'redux'
 // local imports
-import createResponsiveStoreEnhancer from 'util/createResponsiveStoreEnhancer'
+import {responsiveStoreEnhancer, createResponsiveStoreEnhancer, createResponsiveStateReducer} from 'redux-responsive'
 
 
 describe('PerformanceMode handlers', function () {
-    it('respects the calculateInitialState option')
+    it('calcuates the initial state by default', function() {
+        // create a store with the default behavior
+        const reducer = combineReducers({
+            browser: responsiveStateReducer,
+        })
+        // create the enhanced store
+        const store = createStore(reducer, responsiveStoreEnhancer)
+        // get the current state of the browser
+        const {browser} = store.getState()
+
+        // make sure the browser is not in its biggest state (should be opened at 500 px)
+        expect(browser.is.infinity).to.be.false
+    })
+
+    it('does not calcuate the initial state when flagged', function() {
+        // create a store with the default behavior
+        const reducer = combineReducers({
+            browser: responsiveStateReducer,
+        })
+        // create the enhanced store
+        const store = createStore(reducer, createResponsiveStoreEnhancer({calculateInitialState: false}))
+        // get the current state of the browser
+        const {browser} = store.getState()
+
+        // make sure the browser is not in its biggest state (should be opened at 500 px)
+        expect(browser.is.infinity).to.be.true
+    })
+
     it('resizes only when browser width changes to new media query')
 })
