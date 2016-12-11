4d07dff462a941e3cea99de04052acda89a3792d
Alec Aivazis <alec@aivazis.com>
2016-12-11 13:39:27 -0800

    added missing dependency

--- config/karma.js
+++ config/karma.js
@@ -67,7 +67,7 @@ module.exports = function (config) {
         // test results reporter to use
         // possible values: 'dots', 'progress'
         // available reporters: https://npmjs.org/browse/keyword/karma-reporter
-        reporters: ['mocha', 'coveralls'],
+        reporters: ['mocha', 'coverage', 'coveralls'],
 
         // web server port
         port: 9876,
