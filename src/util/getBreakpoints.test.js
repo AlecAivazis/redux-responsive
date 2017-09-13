592146d70c05cfeaaf8d5834d96c5f574d983d6c
Shane Cavaliere <shane@shanecavaliere.com>
2017-09-12 23:21:40 -0600

    added tests for immutable Record root state
    in addition to existing tests for immutable Map root state

--- src/util/getBreakpoints.test.js
+++ src/util/getBreakpoints.test.js
@@ -1,6 +1,7 @@
 // third party imports
 import {createStore, combineReducers} from 'redux'
-import { combineReducers as immutableCombine } from 'redux-immutablejs'
+import { combineReducers as immutableCombine } from 'redux-immutable'
+import { Record } from 'immutable'
 // local imports
 import getBreakpoints from './getBreakpoints'
 import createReducer, {defaultBreakpoints} from './createReducer'
@@ -19,11 +20,25 @@ describe('Breakpoint discovery', function () {
     })
 
 
-    it('Can find immutable js reducers', function() {
+    it('Can responsive state in find Immutable.js Map root state', function() {
         // create a redux store with the reducer at the root
         const store = createStore(immutableCombine({
             browser: reducer
         }))
+
+        // make sure we could retrieve the default breakpoints from the store
+        expect(getBreakpoints(store)).toBe(defaultBreakpoints)
+    })
+
+
+    it('Can responsive state in find Immutable.js Record root state', function() {
+        const StateRecord = Record({
+          browser: undefined
+        })
+        // create a redux store with the reducer at the root
+        const store = createStore(immutableCombine({
+            browser: reducer
+        }, StateRecord))
 
         // make sure we could retrieve the default breakpoints from the store
         expect(getBreakpoints(store)).toBe(defaultBreakpoints)
