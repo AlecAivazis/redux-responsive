085370625b13401029de91cae18ac51fda502855
Igor Korolev <missterr@gmail.com>
2018-07-16 21:49:10 +0400

    format

--- types/tests.ts
+++ types/tests.ts
@@ -3,64 +3,66 @@ import {
     createResponsiveStoreEnhancer,
     responsiveStateReducer,
     responsiveStoreEnhancer,
-    IBrowser, calculateResponsiveState, IBreakPoints,
-} from "redux-responsive";
-import { Action, createStore } from "redux";
+    IBrowser,
+    calculateResponsiveState,
+    IBreakPoints,
+} from 'redux-responsive'
+import { Action, createStore } from 'redux'
 
 // $ExpectType IResponsiveEnhancer
-createResponsiveStoreEnhancer();
+createResponsiveStoreEnhancer()
 
 // $ExpectType IResponsiveEnhancer
-createResponsiveStoreEnhancer({});
+createResponsiveStoreEnhancer({})
 
 // $ExpectType string
-createStore(responsiveStateReducer, responsiveStoreEnhancer).getState().orientation;
+createStore(responsiveStateReducer, responsiveStoreEnhancer).getState().orientation
 
 // $ExpectType boolean
-createStore(responsiveStateReducer).getState().greaterThan.small;
+createStore(responsiveStateReducer).getState().greaterThan.small
 
 // $ExpectType "redux-responsive/CALCULATE_RESPONSIVE_STATE"
-calculateResponsiveState(window).type;
+calculateResponsiveState(window).type
 
-const customBreaks: IBreakPoints<"big" | "veryBig" | "superBig" | "print"> = {
+const customBreaks: IBreakPoints<'big' | 'veryBig' | 'superBig' | 'print'> = {
     big: 500,
     veryBig: 5000,
     superBig: 50000,
-    print: "print",
-};
-const reducer = createResponsiveStateReducer(customBreaks);
-const store = createStore(reducer);
+    print: 'print',
+}
+const reducer = createResponsiveStateReducer(customBreaks)
+const store = createStore(reducer)
 // $ExpectType boolean
-store.getState().greaterThan.superBig;
+store.getState().greaterThan.superBig
 // $ExpectType boolean
-store.getState().greaterThan.print;
+store.getState().greaterThan.print
 // $ExpectError
-store.getState().greaterThan.small;
+store.getState().greaterThan.small
 
 const reducer2 = createResponsiveStateReducer(void 0, {
     extraFields(s) {
         // $ExpectType IBrowser<IBreakPoints<BreakPointsDefaultNames>>
-        s;
-        return { foo: 1 };
-    }
-});
-const store2 = createStore(reducer2);
+        s
+        return { foo: 1 }
+    },
+})
+const store2 = createStore(reducer2)
 // $ExpectType number
-store2.getState().foo;
+store2.getState().foo
 // $ExpectError
-store2.getState().bar;
+store2.getState().bar
 
 const reducer3 = createResponsiveStateReducer(customBreaks, {
     extraFields(s) {
         // $ExpectType IBrowser<IBreakPoints<"big" | "veryBig" | "superBig" | "print">>
-        s;
-        return { foo: 1 };
-    }
-});
-const store3 = createStore(reducer3);
+        s
+        return { foo: 1 }
+    },
+})
+const store3 = createStore(reducer3)
 // $ExpectType number
-store3.getState().foo;
+store3.getState().foo
 // $ExpectError
-store3.getState().bar;
+store3.getState().bar
 // $ExpectType boolean
-store3.getState().greaterThan.print;
+store3.getState().greaterThan.print
